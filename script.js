070567ebc8a0e4417e92fbe13858e97425fae800
thejulianflores <github@thejulianflores.com>
2021-11-16 00:57:27 -0600

    Add a reset button

--- script.js
+++ script.js
@@ -68,28 +68,78 @@ function playRound(player, computer){
 function chooseWinner(outcome){
 
     gameText = document.getElementById('game-text')
-    message = document.createElement('p')
-    message.classList.add('outcome')
+ 
 
     playerWins = document.getElementById('playerWins')
     computerWins = document.getElementById('computerWins')
 
     if(outcome==='win'){
-        message.textContent = ('The player won!')
         playerWins.textContent = (parseInt(playerWins.textContent) + 1)
+        if(playerWins.textContent == 5){
+            gameEnd('player');
+        }
     }
-    else if(outcome==='lose'){
-        message.textContent = ('The computer won!')
-        computerWins.textContent = (parseInt(computerWins.textContent) + 1)
 
+    else if(outcome==='lose'){
+        computerWins.textContent = (parseInt(computerWins.textContent) + 1)
+        if(computerWins.textContent == 5){
+            gameEnd('computer');
+        }
     }
-    else  
-        message.textContent = ('Tie game!')
+
+ }
+
+
+function gameEnd(winner){
+
+    gameText = document.getElementById('game-text')
+    message = document.createElement('p')
+    message.classList.add('outcome')
+
+    playerWins = parseInt(document.getElementById('playerWins')).textContent
+    computerWins = parseInt(document.getElementById('computerWins')).textContent
+
+    if(winner==='player'){
+        message.textContent = ('The player won!')
+    }
+    else if(winner==='computer'){
+        message.textContent = ('The computer won!')
+    }
 
     gameText.appendChild(message)
+    tryAgain();
 }
 
+function pauseBtns() {
+    let rockBtn = document.getElementById('rock');
+    let paperBtn = document.getElementById('paper')
+    let scissorBtn = document.getElementById('scissors')
+    let newRock = rockBtn.cloneNode(true)
+    let newPaper = paperBtn.cloneNode(true)
+    let newScissors = scissorBtn.cloneNode(true)
 
+    rockBtn.parentNode.replaceChild(newRock, rockBtn)
+    paperBtn.parentNode.replaceChild(newPaper, paperBtn)
+    scissorBtn.parentNode.replaceChild(newScissors, scissorBtn)
+}
+
+function tryAgain(){
+    
+    pauseBtns()
+
+    winnerText = document.getElementById('game-text')
+    resetButton = document.createElement('button')
+    resetButton.classList.add('reset')
+    resetButton.textContent = 'Reset';
+
+    winnerText.appendChild(resetButton)
+
+    resetButton.addEventListener('click', function (e){
+        initGame();
+    })
+
+
+}
 
 /**
 * Function that will play 5 games in a row. Determines
@@ -97,34 +147,62 @@ function chooseWinner(outcome){
 * with the winner.
 */
 function game(buttonChosen){
-    let playerWins = 0
-    let computerWins = 0
 
-    gameText = document.getElementById('game-text')
+
+
+
+    gameText = document.getElementById('game-text')    //resets the text box
     while(gameText.firstChild){
         gameText.removeChild(gameText.firstChild)
     }
 
     const computerChoice = computerPlay();
     const playerChoice = buttonChosen;
+
     outcome = playRound(playerChoice, computerChoice)
-        
-    chooseWinner(outcome)
+    
+    if(outcome ==='win') {
+        chooseWinner(outcome);
+    }
+    else if(outcome ==='lose') {
+        chooseWinner(outcome);
+    }
+
+   
+    
+}
+
+function initGame(){
+
+    playerWins = document.getElementById('playerWins')
+    computerWins = document.getElementById('computerWins')
+    message = document.getElementById('game-text')
+
+    message.innerHTML = ''
+
+    
+    playerWins.textContent = '0'
+    computerWins.textContent = '0'
+
+    const rock = document.getElementById('rock');
+    const paper = document.getElementById('paper');
+    const scissors = document.getElementById('scissors');
+    
+    
+    
+    
+    rock.addEventListener('click', function (e){
+        setTimeout(() => (game(e.target.id)), 500)
+    })
+    
+    paper.addEventListener('click', function (e){
+        setTimeout(() => (game(e.target.id)), 500)
+    })
+    
+    scissors.addEventListener('click', function (e) {
+        setTimeout(() => (game(e.target.id)), 500)
+    })
 }
 
 
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissors = document.getElementById('scissors');
-
-rock.addEventListener('click', function (e){
-    setTimeout(() => (game(e.target.id)), 500)
-})
-
-paper.addEventListener('click', function (e){
-    setTimeout(() => (game(e.target.id)), 500)
-})
-
-scissors.addEventListener('click', function (e) {
-    setTimeout(() => (game(e.target.id)), 500)
-})
+initGame();
